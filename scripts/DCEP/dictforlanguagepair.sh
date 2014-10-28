67362694380adea7c1b9d7ba929ce4fa34ae395e
Daniel Varga <daniel@mokk.bme.hu>
2014-10-28 17:41:13 +0100

    get rid of too long sentences

--- scripts/DCEP/dictforlanguagepair.sh
+++ scripts/DCEP/dictforlanguagepair.sh
@@ -8,7 +8,11 @@ l2=$2
 
 bi=langpairs/biqf/$l1-$l2
 
-cat $bi | cut -f1 > tmp.l1
-cat $bi | cut -f2 > tmp.l2
+# I forgot to do this filtering step in the biqf creation step.
+# I'm lazy and do it with byte length
+cat $bi | awk '(length($0)<1000)' > tmp.bi
 
-./acquisScripts/scripts/coocc.forAcquis -mc20 -ms40 tmp.l1 tmp.l2 2> langpairs/autodict.log/$l1-$l2 > langpairs/autodict/$l1-$l2.dic
+cat tmp.bi | cut -f1 > tmp.l1
+cat tmp.bi | cut -f2 > tmp.l2
+
+./acquisScripts/scripts/coocc.forAcquis -mc10 -ms40 tmp.l1 tmp.l2 2> langpairs/autodict.log/$l1-$l2 > langpairs/autodict/$l1-$l2.dic
